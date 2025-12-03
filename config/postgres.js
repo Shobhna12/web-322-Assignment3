51dd5ac62bced3b9476f7af556029da4ac16f2fe
Shobhna12 <shobhna075@gmail.com>
2025-12-02 21:33:22 -0500

    sql commit for Assignment 3

--- config/postgres.js
+++ config/postgres.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+    dialect: "postgres",
+    dialectModule: require("pg"),
     logging: false   // hide SQL logs
 });
 
